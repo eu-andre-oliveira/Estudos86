195f9f5ba327116a02514e4ed5f6f62317bbfa72
Andre <eu.andre.oliveira@hotmail.com>
2023-06-15 21:03:40 -0300

    criando controller de contas

---
+++ src/GerenciadorContas86/Api/Controllers/ContasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class ContasController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}

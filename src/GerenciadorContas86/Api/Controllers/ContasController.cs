8c0289bb0e367b29ba793c3ef3f5a91c244a37e9
Andre <eu.andre.oliveira@hotmail.com>
2023-06-27 18:48:56 -0300

    criando a controller

--- src/GerenciadorContas86/Api/Controllers/ContasController.cs
+++ src/GerenciadorContas86/Api/Controllers/ContasController.cs
@@ -2,8 +2,11 @@
 
 namespace Api.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ContasController : ControllerBase
     {
+        [HttpGet("Listar")]
         public IActionResult Index()
         {
             return Ok();
